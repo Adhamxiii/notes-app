aa28391ab6a1b6b715d1268f779fe8e4fea3a611
Adhamxiii <adhamxiii22@gmail.com>
2025-08-28 13:19:14 +0300

    compelete home page

---
+++ components/footer.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import { Logo } from "./logo";
+
+const links = [
+  {
+    title: "Features",
+    href: "#",
+  },
+  {
+    title: "Solution",
+    href: "#",
+  },
+  {
+    title: "Customers",
+    href: "#",
+  },
+  {
+    title: "Pricing",
+    href: "#",
+  },
+  {
+    title: "Help",
+    href: "#",
+  },
+  {
+    title: "About",
+    href: "#",
+  },
+];
+
+export default function FooterSection() {
+  return (
+    <footer className="py-16 md:py-32">
+      <div className="mx-auto max-w-5xl px-6">
+        <Link href="/" aria-label="go home" className="mx-auto block size-fit">
+          <Logo />
+        </Link>
+
+
+        <span className="text-muted-foreground block text-center text-sm">
+          {" "}
+          © {new Date().getFullYear()} NoteApp, All rights reserved
+        </span>
+      </div>
+    </footer>
+  );
+}
